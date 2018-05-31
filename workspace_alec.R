950d9ed73e2f58586430dbb8e637f3f22599e29c
Alec Bergen <bergenal@uw.edu>
2018-05-31 11:33:32 -0700

    Final

--- workspace_alec.R
+++ workspace_alec.R
@@ -1,10 +1,6 @@
 library(shiny)
 library(dplyr)
-library(plot3D)
-library(RColorBrewer)
 library(plotly)
-library(viridis)
-library(leaflet)
 
 # Functions
 swap_date <- function(input_date) {
@@ -25,17 +21,19 @@ id_data$category_id <- as.integer(id_data$category_id)
 joined_data <- left_join(project_data, id_data)
 
 ui <- fluidPage(
-  titlePanel("Dates and date ranges"),
+  titlePanel("Composition of Trending Videos by Category"),
   sidebarLayout(
   sidebarPanel(
   dateInput('date',
-            label = 'Date input (2017/11/14 - 2018/03/30):',
+            label = paste('Date input (2017/11/14 - 2018/03/30):'),
             value = "2017-11-14",
             format = "yyyy-mm-dd"
   )),
   mainPanel(
   plotlyOutput("graph"),
-  tableOutput("table")
+  dataTableOutput("table"),
+  htmlOutput('description')
+  
 )
 )
 )
@@ -54,25 +52,26 @@ server <- function(input, output) {
         comment_count = sum(comment_count,na.rm=T),
         views = sum(as.numeric(views),na.rm=T)
       )
+    # Counts the number of occurances of each group
     n_occurance <- joined_data %>% filter(trending_date == substr(swap_date(as.character(input$date)), 3, 10)) %>%
       group_by(category_name) %>%  mutate(count = 1) %>%  summarize(total = sum(count))
-    
-    arranged_data <- joined_data %>% arrange(-likes) %>% mutate(ranking = rownames(joined_data)) %>%
-      select(category_name, ranking)
+    # # Arranges data by likes and constructs datafrane of rankings by likes
+    # arranged_data <- joined_data %>% arrange(-likes) %>% mutate(ranking = rownames(joined_data)) %>%
+    #   select(category_name, ranking)
     
     # Joins data sets
     scaled_data <- left_join(q_data, n_occurance)
-    
+    # Arranges data by likes and constructs datafrane of rankings by likes
     arranged_data <- scaled_data %>% arrange(-likes) %>% mutate(ranking = rownames(scaled_data)) %>%
       select(category_name, ranking)
-    
+    # Joins data sets
     scaled_data <- left_join(scaled_data, arranged_data)
-
+    # Creates ratio
     x <- scaled_data$views / scaled_data$total
     y <- scaled_data$likes / scaled_data$total
     z <- scaled_data$comment_count / scaled_data$total
     id <- scaled_data$ranking
-    
+    # Construct and return plotly graph
     p <- plot_ly(scaled_data, x = ~x, y = ~y, text = ~category_name, type = 'scatter', size= ~comment_count, mode = 'markers',
                  color = ~category_name, opacity = .5,
                  marker = list(sizemode = 'diameter')) %>%
@@ -82,7 +81,8 @@ server <- function(input, output) {
              showlegend = FALSE)
     p
   })
-  output$table<- renderTable({
+  # Same code as above, but as a data table
+  output$table<- renderDataTable({
     
     for_fun <- joined_data  %>% mutate(count = 1) %>% filter(trending_date == substr(swap_date(as.character(input$date)), 3, 10))%>%
       group_by(category_name) %>% summarize(total = sum(count,na.rm = T))
@@ -101,7 +101,6 @@ server <- function(input, output) {
     arranged_data <- joined_data %>% arrange(-likes) %>% mutate(ranking = rownames(joined_data)) %>%
       select(category_name, ranking)
     
-    # Joins data sets
     scaled_data <- left_join(q_data, n_occurance)
     
     arranged_data <- scaled_data %>% arrange(-likes) %>% mutate(ranking = rownames(scaled_data)) %>%
@@ -109,6 +108,16 @@ server <- function(input, output) {
     
     scaled_data <- left_join(scaled_data, arranged_data)
     scaled_data
+  })
+  output$description <- renderUI({
+    code_break <- "<br><br>"
+    str1 <- "The graphs visualizes the number of likes per view as a scatter plot. The size of the dot represents
+    the numbers of views that the video reached when the video became a trending video on YouTube. The functionality
+    of this application allows for analysis of likes/view scaled by comment count for each day (2017/11/14 - 2018/03/30)"
+    str2 <- "This graph gives the user the understanding of the make-up of a trending video. This sort of analysis could
+    be used to gain insight into what it takes to make their video become a trending video in their marketed category. While each category's
+    ratio varies per day of the week, there are general trends that emerge for each video, and the graph has a rough positive correlation."
+    HTML(paste(code_break, str1, str2, sep="<br><br>"))
   })
 }
 
