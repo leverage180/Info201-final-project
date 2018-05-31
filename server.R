915937c5785cecfa007dcfc45849cc387c6e268c
Alec Bergen <bergenal@uw.edu>
2018-05-31 12:46:33 -0700

    Finished my part

--- server.R
+++ server.R
@@ -7,9 +7,9 @@
 #    http://shiny.rstudio.com/
 #
 
-# unwanted_array = list(    'Å '='S', 'Å¡'='s', 'Å½'='Z', 'Å¾'='z', 'Ã'='A', 'Ã'='A', 'Ã'='A', 'Ã'='A', 'Ã'='A', 'Ã'='A', 'Ã'='A', 'Ã'='C', 'Ã'='E', 'Ã'='E',
+# unwanted_array = list(    'Å '='S', 'Å¡'='s', 'Å½'='Z', 'Å¾'='z', 'Ã'='A', 'Ã'='A', 'Ã'='A', 'Ã'='A', 'Ã'='A', 'Ã'='A', 'Ã'='A', 'Ã'='C', 'Ã'='E', 'Ã'='E',
 #                            'Ã'='E', 'Ã'='E', 'Ã'='I', 'Ã'='I', 'Ã'='I', 'Ã'='I', 'Ã'='N', 'Ã'='O', 'Ã'='O', 'Ã'='O', 'Ã'='O', 'Ã'='O', 'Ã'='O', 'Ã'='U',
-#                            'Ã'='U', 'Ã'='U', 'Ã'='U', 'Ã'='Y', 'Ã'='B', 'Ã'='Ss', 'Ã '='a', 'Ã¡'='a', 'Ã¢'='a', 'Ã£'='a', 'Ã¤'='a', 'Ã¥'='a', 'Ã¦'='a', 'Ã§'='c',
+#                            'Ã'='U', 'Ã'='U', 'Ã'='U', 'Ã'='Y', 'Ã'='B', 'Ã'='Ss', 'Ã '='a', 'Ã¡'='a', 'Ã¢'='a', 'Ã£'='a', 'Ã¤'='a', 'Ã¥'='a', 'Ã¦'='a', 'Ã§'='c',
 #                            'Ã¨'='e', 'Ã©'='e', 'Ãª'='e', 'Ã«'='e', 'Ã¬'='i', 'Ã?'='i', 'Ã®'='i', 'Ã¯'='i', 'Ã°'='o', 'Ã±'='n', 'Ã²'='o', 'Ã³'='o', 'Ã´'='o', 'Ãµ'='o',
 #                            'Ã¶'='o', 'Ã¸'='o', 'Ã¹'='u', 'Ãº'='u', 'Ã»'='u', 'Ã½'='y', 'Ã½'='y', 'Ã¾'='b', 'Ã¿'='y' )
 # 
@@ -78,6 +78,25 @@ source("ui.R")
 
 library(shiny)
 library(plotly)
+
+# Reads data
+project_data <- read.csv('youtube-new/USvideos.csv', stringsAsFactors = F)
+project_data$trending_date <- gsub("[.]", "/", project_data$trending_date)
+id_data <- read.csv('youtube-new/category_id.csv', stringsAsFactors = F)
+# Names & Joins
+colnames(id_data) <- c("category_id","category_name")
+# Coerces int
+id_data$category_id <- as.integer(id_data$category_id)
+joined_data <- left_join(project_data, id_data)
+
+swap_date <- function(input_date) {
+  year <- substr(input_date,1,4)
+  month <- substr(input_date,6,7)
+  day <-  substr(input_date,9,10)
+  return_date <- paste0(year, "/", day, "/", month)
+  return_date
+}
+
 ############################################################################
 ################### Anh-Minh ###############################################
 ############################################################################
@@ -115,7 +134,7 @@ df_c <- df_u[!(df_u$word %in% plain_words),]
 #############################################################################################################
 
 shinyServer(function(input, output) {
-
+  
   ############################################################################
   ###################### CONNOR's WORK #######################################
   ############################################################################
@@ -156,9 +175,9 @@ shinyServer(function(input, output) {
     p <- ggplotly(p, tooltip = "y")
     p
   })
-
- ################################################################ 
-######################### ANH-MINH'S WORK #######################
+  
+  ################################################################ 
+  ######################### ANH-MINH'S WORK #######################
   ###############################################################
   decipher <- reactive({
     if(input$choice == "positive" | input$choice == "negative") {
@@ -254,5 +273,86 @@ shinyServer(function(input, output) {
       labs(title = paste(input$region, "data"),
            x = "Category",
            y = "Percentage")
+  })
+  # Main Graph
+  output$graph <- renderPlotly({
+    
+    for_fun <- joined_data  %>% mutate(count = 1) %>% filter(trending_date == substr(swap_date(as.character(input$date)), 3, 10))%>%
+      group_by(category_name) %>% summarize(total = sum(count))
+    
+    # Sums category totals for specific date
+    q_data <- joined_data %>% filter(trending_date == substr(swap_date(as.character(input$date)), 3, 10)) %>%
+      group_by(category_name) %>%  summarize(
+        likes = sum(likes),
+        dislikes = sum(dislikes),
+        comment_count = sum(comment_count),
+        views = sum(views)
+      )
+    # Counts the number of occurances of each group
+    n_occurance <- joined_data %>% filter(trending_date == substr(swap_date(as.character(input$date)), 3, 10)) %>%
+      group_by(category_name) %>%  mutate(count = 1) %>%  summarize(total = sum(count))
+    # # Arranges data by likes and constructs datafrane of rankings by likes
+    # arranged_data <- joined_data %>% arrange(-likes) %>% mutate(ranking = rownames(joined_data)) %>%
+    #   select(category_name, ranking)
+    
+    # Joins data sets
+    scaled_data <- left_join(q_data, n_occurance)
+    # Arranges data by likes and constructs datafrane of rankings by likes
+    arranged_data <- scaled_data %>% arrange(-likes) %>% mutate(ranking = rownames(scaled_data)) %>%
+      select(category_name, ranking)
+    # Joins data sets
+    scaled_data <- left_join(scaled_data, arranged_data)
+    # Creates ratio
+    x <- scaled_data$views / scaled_data$total
+    y <- scaled_data$likes / scaled_data$total
+    z <- scaled_data$comment_count / scaled_data$total
+    id <- scaled_data$ranking
+    # Construct and return plotly graph
+    p <- plot_ly(scaled_data, x = ~x, y = ~y, text = ~category_name, type = 'scatter', size= ~comment_count, mode = 'markers',
+                 color = ~category_name, opacity = .5,
+                 marker = list(sizemode = 'diameter')) %>%
+      layout(title = paste('Ratio of likes to dislikes by category.', '\n', 'Size scaled for comment count.', '\n'),
+             xaxis = list(title ="Views", showgrid = FALSE),
+             yaxis = list(title = "Likes", showgrid = FALSE),
+             showlegend = FALSE)
+    p
+  })
+  # Same code as above, but as a data table
+  output$table<- renderDataTable({
+    
+    for_fun <- joined_data  %>% mutate(count = 1) %>% filter(trending_date == substr(swap_date(as.character(input$date)), 3, 10))%>%
+      group_by(category_name) %>% summarize(total = sum(count))
+    
+    # Sums category totals for specific date
+    q_data <- joined_data %>% filter(trending_date == substr(swap_date(as.character(input$date)), 3, 10)) %>%
+      group_by(category_name) %>%  summarize(
+        likes = sum(likes),
+        dislikes = sum(dislikes),
+        comment_count = sum(comment_count),
+        views = sum(views)
+      )
+    n_occurance <- joined_data %>% filter(trending_date == substr(swap_date(as.character(input$date)), 3, 10)) %>%
+      group_by(category_name) %>%  mutate(count = 1) %>%  summarize(total = sum(count))
+    
+    arranged_data <- joined_data %>% arrange(-likes) %>% mutate(ranking = rownames(joined_data)) %>%
+      select(category_name, ranking)
+    
+    scaled_data <- left_join(q_data, n_occurance)
+    
+    arranged_data <- scaled_data %>% arrange(-likes) %>% mutate(ranking = rownames(scaled_data)) %>%
+      select(category_name, ranking)
+    
+    scaled_data <- left_join(scaled_data, arranged_data)
+    scaled_data
+  })
+  output$description_alec <- renderUI({
+    code_break <- "<br><br>"
+    str1 <- "The graphs visualizes the number of likes per view as a scatter plot. The size of the dot represents
+    the numbers of views that the video reached when the video became a trending video on YouTube. The functionality
+    of this application allows for analysis of likes/view scaled by comment count for each day (2017/11/14 - 2018/03/30)"
+    str2 <- "This graph gives the user the understanding of the make-up of a trending video. This sort of analysis could
+    be used to gain insight into what it takes to make their video become a trending video in their marketed category. While each category's
+    ratio varies per day of the week, there are general trends that emerge for each video, and the graph has a rough positive correlation."
+    HTML(paste(code_break, str1, str2, sep="<br><br>"))
   })
 })
--- ui.R
+++ ui.R
@@ -15,6 +15,16 @@ library(plotly)
 library(tidyr)
 library(tidytext)
 
+# Reads data
+project_data <- read.csv('youtube-new/USvideos.csv', stringsAsFactors = F)
+project_data$trending_date <- gsub("[.]", "/", project_data$trending_date)
+id_data <- read.csv('youtube-new/category_id.csv', stringsAsFactors = F)
+# Names & Joins
+colnames(id_data) <- c("category_id","category_name")
+# Coerces int
+id_data$category_id <- as.integer(id_data$category_id)
+joined_data <- left_join(project_data, id_data)
+
 youtube_data <- read.csv("data/USvideos.csv", stringsAsFactors = FALSE)
 
 youtube_data <- youtube_data %>% 
@@ -22,8 +32,8 @@ youtube_data <- youtube_data %>%
 
 youtube_data <- youtube_data %>% 
   mutate("hour_publish" = if_else(as.numeric(substring(publish_time, 12, 13)) < 7, 
-                                    24 - (7 - as.numeric(substring(publish_time, 12, 13))), 
-                                    as.numeric(substring(publish_time, 12, 13)) - 7))
+                                  24 - (7 - as.numeric(substring(publish_time, 12, 13))), 
+                                  as.numeric(substring(publish_time, 12, 13)) - 7))
 
 youtube_data <- youtube_data %>% 
   mutate("day_of_week" = weekdays(date_publish))
@@ -39,39 +49,39 @@ shinyUI(fluidPage(
   
   tabsetPanel(type = "tabs",
               tabPanel("Welcome",
-                tabsetPanel(
-                  tabPanel("Credit",
-                    p("On this site, users will see breakdown of videos from YouTube's Trending Page. We wish to explore whether a pattern exists amongst the videos
-                      that climb their way onto one of YouTube's most promiment tabs, if not the most promiment. We pulled the data from Kaggle. The dataset was produced
-                      and updated by a user named 'Mitchell J'. The dataset is called 'Trending YouTube Video Statistics. We are using version 83 of the USVideos and other
-                      countries.") 
-                  ),
-                  tabPanel("Thesis",
-                    p("For this project, we will present visualizations and explanation that seek to answer the question if there are certain common attributes amongst videos
-                      which appear on Trending. If there is no common or all the traits are proportional in appearance, such as every word has a similar chance of appearing, then
-                      we would lean towards rejecting the notion that only certain types of videos appear regularly on the YouTube Trending. As users will see, there does appear to be 
-                      some behaviors that indicate that there are patterns available for creators to use to appear on YouTube's Trending page. Thus, for this project, we want to find
-                      evidence that supports the idea that there are certain types of videos that appear on the Trending Page, signaling that they hold advantages over other videos on
-                      YouTube."),
-                    p(htmlOutput("trend_link"))
-                  ),
-                  tabPanel("Group Information",
-                    h3("/0 [Divide By Zero]"),
-                    h4("Anh-Minh Nguyen"),
-                    h4("Connor Carlson"),
-                    h4("Thomas Harding"),
-                    h4("Alec Bergen"),
-                    h5("Date Produced: 5/30/2018")
-                  )
-                )
-              ),
+                       tabsetPanel(
+                         tabPanel("Credit",
+                                  p("On this site, users will see breakdown of videos from YouTube's Trending Page. We wish to explore whether a pattern exists amongst the videos
+                                    that climb their way onto one of YouTube's most promiment tabs, if not the most promiment. We pulled the data from Kaggle. The dataset was produced
+                                    and updated by a user named 'Mitchell J'. The dataset is called 'Trending YouTube Video Statistics. We are using version 83 of the USVideos and other
+                                    countries.") 
+                                  ),
+                         tabPanel("Thesis",
+                                  p("For this project, we will present visualizations and explanation that seek to answer the question if there are certain common attributes amongst videos
+                                    which appear on Trending. If there is no common or all the traits are proportional in appearance, such as every word has a similar chance of appearing, then
+                                    we would lean towards rejecting the notion that only certain types of videos appear regularly on the YouTube Trending. As users will see, there does appear to be 
+                                    some behaviors that indicate that there are patterns available for creators to use to appear on YouTube's Trending page. Thus, for this project, we want to find
+                                    evidence that supports the idea that there are certain types of videos that appear on the Trending Page, signaling that they hold advantages over other videos on
+                                    YouTube."),
+                                  p(htmlOutput("trend_link"))
+                                  ),
+                         tabPanel("Group Information",
+                                  h3("/0 [Divide By Zero]"),
+                                  h4("Anh-Minh Nguyen"),
+                                  h4("Connor Carlson"),
+                                  h4("Thomas Harding"),
+                                  h4("Alec Bergen"),
+                                  h5("Date Produced: 5/30/2018")
+                         )
+                         )
+                         ),
               tabPanel("Time",
-                sidebarLayout(
-                  sidebarPanel(
+                       sidebarLayout(
+                         sidebarPanel(
                            
-                    selectInput("days",
-                                "Day of the Week",
-                                choices = c("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"))
+                           selectInput("days",
+                                       "Day of the Week",
+                                       choices = c("Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"))
                          ),
                          
                          mainPanel(
@@ -79,51 +89,69 @@ shinyUI(fluidPage(
                            plotlyOutput("time_plot"),
                            plotlyOutput("to_trending")
                          )
-              )  
-        ),
-        tabPanel("Titles",
-          sidebarLayout(
-            sidebarPanel(
-              radioButtons("choice",  label = strong("Word Searches"), 
-                          choices = list("Default" = "default",
-                                         "Positive" = "positive",
-                                         "Negative" = "negative",
-                                         "All" = "all"))
-            ),
-            mainPanel(
-              tabsetPanel(type = "tabs",
-                          tabPanel("Breakdown",
-                                   h2("What are the most common words used in YouTube videos on the Trending page?"),
-                                   p("Titles are used to briefly describe and label a video. With the boom in video production and content, however,
-                                     it becomes hard to stand apart. In this section, we will explore what words are commonly found in these videos
-                                     to uncover any correlations."),
-                                   p("After exploring the data, users may find that many large studios basically control the Trending Page. They seem to push
-                                     many official trailers and music videos that dominate the Trending page. So, it seems as if the key to being famous on YouTube 
-                                     as of the moment is to be immenseely powerful or famous already. A plethora of words are related to a 'bougie'
-                                     lifestyle as well.")
-                                   ),
-                          tabPanel("Word Chart",
-                                   plotOutput('wordBar', click = "my_click"),
-                                   verbatimTextOutput('wordText'),
-                                   p(htmlOutput("description"))
-                          )
-                                   )
-                          )
-          )  
-        ),
-        tabPanel("Categories",
-                 sidebarLayout(
-                   sidebarPanel(
-                     selectInput("region",  label = strong("Region"), 
-                                 choices = list(America = "US", Canada = "CA", Germany = "DE", France = "FR",
-                                                England = "GB"))
-                   ),
-                   mainPanel(
-                     plotOutput("category_plot", click = "my_plot_click")
-                   )
-                 )  
-        )
-      )
-    )
-  )
-
+                       )  
+              ),
+              tabPanel("Titles",
+                       sidebarLayout(
+                         sidebarPanel(
+                           radioButtons("choice",  label = strong("Word Searches"), 
+                                        choices = list("Default" = "default",
+                                                       "Positive" = "positive",
+                                                       "Negative" = "negative",
+                                                       "All" = "all"))
+                         ),
+                         mainPanel(
+                           tabsetPanel(type = "tabs",
+                                       tabPanel("Breakdown",
+                                                h2("What are the most common words used in YouTube videos on the Trending page?"),
+                                                p("Titles are used to briefly describe and label a video. With the boom in video production and content, however,
+                                                  it becomes hard to stand apart. In this section, we will explore what words are commonly found in these videos
+                                                  to uncover any correlations."),
+                                                p("After exploring the data, users may find that many large studios basically control the Trending Page. They seem to push
+                                                  many official trailers and music videos that dominate the Trending page. So, it seems as if the key to being famous on YouTube 
+                                                  as of the moment is to be immenseely powerful or famous already. A plethora of words are related to a 'bougie'
+                                                  lifestyle as well.")
+                                                ),
+                                       tabPanel("Word Chart",
+                                                plotOutput('wordBar', click = "my_click"),
+                                                verbatimTextOutput('wordText'),
+                                                p(htmlOutput("description"))
+                                       )
+                                                )
+                                       )
+                       )  
+                       ),
+              tabPanel("Categories",
+                       sidebarLayout(
+                         sidebarPanel(
+                           selectInput("region",  label = strong("Region"), 
+                                       choices = list(America = "US", Canada = "CA", Germany = "DE", France = "FR",
+                                                      England = "GB"))
+                         ),
+                         mainPanel(
+                           plotOutput("category_plot", click = "my_plot_click")
+                         )
+                       )  
+              ),
+              tabPanel("Composition",
+                       fluidPage(
+                         titlePanel("Composition of Trending Videos by Category"),
+                         sidebarLayout(
+                           sidebarPanel(
+                             dateInput('date',
+                                       label = paste('Date input (2017/11/14 - 2018/03/30):'),
+                                       value = "2017-11-14",
+                                       format = "yyyy-mm-dd"
+                             )),
+                           mainPanel(
+                             plotlyOutput("graph"),
+                             dataTableOutput("table"),
+                             htmlOutput('description_alec')
+                             
+                           )
+                         )
+                       )
+              )
+                       )
+              )
+                       )
